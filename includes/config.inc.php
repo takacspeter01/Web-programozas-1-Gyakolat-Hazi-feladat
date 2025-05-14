072410e61641b07eaff3617dabe0c002b2e97ca5
Bliss <161327074+takacspeter01@users.noreply.github.com>
2025-05-14 19:25:31 +0200

    Add files via upload

---
+++ includes/config.inc.php
@@ -0,0 +1,38 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Web Gyakorlat Beadandó',
+);
+
+$fejlec = array(
+	'cim' => 'Autókereskedés',
+    'motto' => 'Legjobb autók legjobb áron!'
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Takács Péter OE09T3, Halasi Zoltán JJNRKU'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+	'autok' => array('fajl' => 'autok', 'szoveg' => 'Autóink', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'üzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(0,1)),
+    
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'feldolgozas' => array('fajl' => 'feldolgozas', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
+
+<?php
+    $MAPPA = './kepek/';
+    $TIPUSOK = array ('.jpg', '.png');
+    $MEDIATIPUSOK = array('image/jpeg', 'image/png');
+    $DATUMFORMA = "Y.m.d. H:i";
+    $MAXMERET = 1000*1000;
+?>
